5dd93f4a482d730639957f62cd70801b1767d3d7
Victor Machado Emerick <emerick_machado@hotmail.com>
2019-09-12 13:21:53 -0300

    Tela de Cadastro finalizada - somente design

--- churrascheduler_front/src/App.js
+++ churrascheduler_front/src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import bbqpattern from './assets/images/bbq-pattern.svg';
 import trincaLogo from './assets/images/trinca-logo.svg';
 import './App.css';
 import Login from './pages/Login/Login';
+import Cadastro from './pages/Cadastro/Cadastro';
+import { isTSConstructorType } from '@babel/types';
 
-function App() {
-    return (
-        <div className="App">
-            <div className="background">
-                <div className="shadowBox">
-                    <img src={bbqpattern} className="top-background" alt="Top-Background" />
-                    <div className="shadow"></div>
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.getFundoLogado = this.getFundoLogado.bind(this);
+    }
+
+    getFundoLogado() {
+        let auth = window.localStorage.getItem("authToken");
+        if (auth !== null) {
+            return <div className="background-branco" />;
+        }
+        return "";
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="background">
+                    <div className="shadowBox">
+                        <img src={bbqpattern} className="top-background" alt="Top-Background" />
+                        <div className="shadow"></div>
+                    </div>
+                    {this.getFundoLogado()}
+                    <h1>Agenda de Churras</h1>
+                    <img src={trincaLogo} className="trinca-logo" />
                 </div>
-                <h1>Agenda de Churras</h1>
-                <img src={trincaLogo} className="trinca-logo" />
+                <div className="content">
+                    <Cadastro />
+                </div>
             </div>
-            <div className="content">
-                <Login />
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
